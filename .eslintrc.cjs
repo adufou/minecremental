fc2fada3663f98b8cf3d940d1baf5bd48e329f22
adufou <antoine.du.fou@gmail.com>
2024-04-15 23:43:25 +0200

    chore: add prettier and eslint, and a script to fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    //   Here is config for react recommended, see : https://github.com/microsoft/TypeScript/issues/56042
+    // Here is config for react recommended, see : https://github.com/microsoft/TypeScript/issues/56042
     "plugin:react/jsx-runtime",
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    // Place this last ! "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
