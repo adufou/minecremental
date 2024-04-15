cb91d1ad5af1306cb35e167bf9c928df6aadd01d
adufou <antoine.du.fou@gmail.com>
2024-04-15 23:57:49 +0200

    chore: apply prettier on all files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    // Here is config for react recommended, see : https://github.com/microsoft/TypeScript/issues/56042
-    "plugin:react/jsx-runtime",
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    // Place this last ! "plugin:prettier/recommended"
-    "plugin:prettier/recommended",
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: [],
-  rules: {
-    'react-hooks/exhaustive-deps': 'off',
-  },
-}
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        // Here is config for react recommended, see : https://github.com/microsoft/TypeScript/issues/56042
+        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        // Place this last ! "plugin:prettier/recommended"
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: [],
+    rules: {
+        'react-hooks/exhaustive-deps': 'off',
+    },
+};
