549e00510def7b4fafb818521e32f8fab6a45c50
adufou <antoine.du.fou@gmail.com>
2024-04-17 16:54:24 +0200

    fix: vite config base repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/minecremental/', // from https://stackoverflow.com/questions/74518887/blank-page-when-deploying-a-react-app-to-github-pages-and-vite when doing https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site
     plugins: [react()],
     resolve: {
         alias: {
