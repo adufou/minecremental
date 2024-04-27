31af285e665b2fef32682fb127e088ca5df2bb3b
adufou <antoine.du.fou@gmail.com>
2024-04-27 11:23:04 +0200

    feat: inventory accordions openned by default

--- src/modules/Inventory/player-inventory.tsx
+++ src/modules/Inventory/player-inventory.tsx
@@ -8,17 +8,42 @@ import { Card } from '@/components/ui/card.tsx';
 import BuildingCell from '@/modules/Inventory/components/building-cell.tsx';
 import InventoryStack from '@/modules/Inventory/components/inventory-stack.tsx';
 import { useBoundStore } from '@/store/store.ts';
+import { useState } from 'react';
+
+type Accordions = 'inventory' | 'village';
 
 function PlayerInventory() {
     const boundStore = useBoundStore();
+
+    const [activeAccordions, setActiveAccordions] = useState<Accordions[]>([
+        'inventory',
+        'village',
+    ]);
+
+    const toggleAccordion = (accordion: Accordions) => {
+        if (activeAccordions.includes(accordion)) {
+            setActiveAccordions(
+                activeAccordions.filter((a) => a !== accordion),
+            );
+        } else {
+            setActiveAccordions([...activeAccordions, accordion]);
+        }
+    };
 
     return (
         <div className='flex flex-0 w-64 m-2 ml-0'>
             <Card className='h-full w-full'>
                 <div className='flex flex-col p-2 gap-1'>
-                    <Accordion type='multiple'>
+                    <Accordion
+                        type='multiple'
+                        value={activeAccordions}
+                    >
                         <AccordionItem value='inventory'>
-                            <AccordionTrigger>Inventory</AccordionTrigger>
+                            <AccordionTrigger
+                                onClick={() => toggleAccordion('inventory')}
+                            >
+                                Inventory
+                            </AccordionTrigger>
                             {Object.entries(boundStore.inventory).map(
                                 ([key, value]) => {
                                     return (
@@ -31,7 +56,11 @@ function PlayerInventory() {
                         </AccordionItem>
 
                         <AccordionItem value='village'>
-                            <AccordionTrigger>Village</AccordionTrigger>
+                            <AccordionTrigger
+                                onClick={() => toggleAccordion('village')}
+                            >
+                                Village
+                            </AccordionTrigger>
                             {Object.entries(boundStore.village).map(
                                 ([key, value]) => {
                                     return (
