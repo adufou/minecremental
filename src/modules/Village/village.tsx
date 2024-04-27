b2b0363d69700251dcad39a1d3c7cb726ce7a3b0
adufou <antoine.du.fou@gmail.com>
2024-04-27 11:45:31 +0200

    chore: reorder sidebar

--- src/modules/Village/village.tsx
+++ src/modules/Village/village.tsx
@@ -1,5 +1,4 @@
 import { Separator } from '@/components/ui/separator.tsx';
-import { Slider } from '@/components/ui/slider.tsx';
 import BuildingsRecipes from '@/constants/buildings-recipes.ts';
 import CraftBuild from '@/modules/Village/components/craft-build.tsx';
 import { useBoundStore } from '@/store/store.ts';
@@ -10,17 +9,7 @@ function Village() {
     return (
         <div className='flex flex-col w-full h-full gap-2 p-2'>
             <div>
-                <span>{boundStore.villagers} villager</span>
-                <br />
-                <Slider
-                    max={1000}
-                    onValueChange={(value: number[]) => {
-                        boundStore.villagers = value[0];
-                    }}
-                    step={1}
-                    value={[boundStore.villagers]}
-                />
-                <span>WIP: this slider is for dev and cheat purpose</span>
+                <span className='text-xl'>{boundStore.villagers} villager</span>
             </div>
             <Separator />
             <div className='grid grid-cols-4 gap-2'>
