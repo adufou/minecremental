a7d0413277d59951fd6d600b785e20e28852fbec
Antoine du FOU <52314805+adufou@users.noreply.github.com>
2024-04-27 19:17:36 +0200

    chore: move forest and village stores to their respective modules (#25)

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,15 @@
 import {
     createTreeSlice,
     ForestSliceCreator,
-} from '@/components/providers/engine/Forest/store/forest-slice-creator.ts';
-import {
-    createVillageSlice,
-    VillageSliceCreator,
-} from '@/components/providers/engine/Village/store/village-slice-creator.ts';
+} from '@/modules/Forest/store/forest-slice-creator.ts';
 import {
     createInventorySlice,
     InventorySliceCreator,
 } from '@/modules/Inventory/store/inventory-slice-creator.ts';
+import {
+    createVillageSlice,
+    VillageSliceCreator,
+} from '@/modules/Village/store/village-slice-creator.ts';
 import { create } from 'zustand';
 
 export type State = ForestSliceCreator &
