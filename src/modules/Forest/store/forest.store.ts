1f5d9790d49185885cc74760416bdb70e49e7d07
adufou <antoine.du.fou@gmail.com>
2024-06-02 22:10:07 +0200

    chore(vue): implement chop by villager, untestable

--- src/modules/Forest/store/forest.store.ts
+++ src/modules/Forest/store/forest.store.ts
@@ -1,6 +1,7 @@
 import { ChopConstants } from '@/modules/Forest/consts';
 import type { ItemStack } from '@/modules/Inventory/models/inventory';
 import { useInventoryStore } from '@/modules/Inventory/store/inventory.store';
+import { useVillageStore } from '@/modules/Village/store/village.store';
 import type { Item, ItemsType } from '@/shared/constants/items';
 import ItemTypes from '@/shared/models/itemTypes';
 import { defineStore } from 'pinia';
@@ -69,6 +70,111 @@ export const useForestStore = defineStore('forest', () => {
         inventoryStore.inventory[item.name] = {
             item,
             size: (inventoryStore.inventory[item.name]?.size ?? 0) + nbChopped,
+        };
+    };
+
+    const chopByVillager = (elapsed: number, item: Item) => {
+        /* Stores uses first ! See https://pinia.vuejs.org/cookbook/composing-stores.html */
+        const inventoryStore = useInventoryStore();
+        const villageStore = useVillageStore();
+
+        /* Action */
+        let nbChopped = 0;
+        let availableVillagers = 0;
+        const initialProgress = chopProgress.value;
+        let totalProgress = initialProgress;
+
+        // Progress in ratio
+        const oneVillagerChopBaseProgress =
+            (elapsed / ChopConstants.BASE_CHOP_DURATION_IN_MS) * 100;
+
+        // const villagers = useBoundStore.getState().villagers;
+        // const newInventory = useBoundStore.getState().inventory;
+
+        availableVillagers = villageStore.villagers;
+
+        // This loop uses tools first
+        for (const [key, value] of Object.entries(
+            inventoryStore.inventory,
+        ) as Array<[keyof ItemsType, ItemStack]>) {
+            if (availableVillagers === 0) {
+                break;
+            }
+
+            if (
+                inventoryStore.inventory[key] !== undefined &&
+                value.item.type === ItemTypes.AXE &&
+                value.size > 0 &&
+                value.item.durability !== undefined &&
+                value.durability !== undefined
+            ) {
+                const usableVillagers = Math.min(
+                    availableVillagers,
+                    value.size,
+                );
+
+                // THIS SUPPOSES ALL VILLAGER SHARE AND USE THE SAME TOOL
+                // If the chop time is 1000ms, the elapsed time is 17ms, and there is 1 villager
+                // If, by click, using exactly 1 durability, we can progress by 10 (%)
+                // Then when the oneVillagerChopBaseProgress is 1.7 [(17/1000) * 100], we can progress by 1.7 (%)
+                // Meaning we did 1.7/10 = 0.17 durability worth of progress for 1 villager
+
+                const oneVillagerDurabilityUsageRelativeToBaseClickProgress =
+                    oneVillagerChopBaseProgress /
+                    ChopConstants.BASE_CHOP_CLICK_PROGRESS;
+
+                // Durability usage
+                const totalDurability =
+                    (value.size - 1) * value.item.durability + value.durability;
+
+                const durabilityUsage =
+                    usableVillagers *
+                    oneVillagerDurabilityUsageRelativeToBaseClickProgress;
+
+                const newTotalDurability = totalDurability - durabilityUsage;
+
+                // Nb chopped computing
+                const progress =
+                    usableVillagers *
+                    oneVillagerChopBaseProgress *
+                    (value.item.multiplier ?? 1);
+
+                totalProgress += progress;
+                availableVillagers -= usableVillagers;
+
+                // Update the stack
+                if (newTotalDurability <= 0) {
+                    inventoryStore.inventory[key] = {
+                        ...value,
+                        durability: value.item.durability,
+                        size: 0,
+                    };
+                } else {
+                    inventoryStore.inventory[key] = {
+                        ...value,
+                        durability: newTotalDurability % value.item.durability,
+                        size: Math.ceil(
+                            newTotalDurability / value.item.durability,
+                        ),
+                    };
+                }
+            }
+        }
+
+        // Once we run out of tools, we use the remaining villagers
+        totalProgress += availableVillagers * oneVillagerChopBaseProgress;
+
+        nbChopped = Math.floor(totalProgress / 100);
+        chopProgress.value = totalProgress % 100;
+
+        const chopSpeedThisTick =
+            (totalProgress - initialProgress) / 100 / (elapsed / 1000);
+
+        inventoryStore.inventory[item.name] = {
+            item,
+            size: (inventoryStore.inventory[item.name]?.size ?? 0) + nbChopped,
+            durability: item.durability,
+            perSecond: chopSpeedThisTick,
         };
     };
 
