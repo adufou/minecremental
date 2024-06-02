5131648b799039818bcb253f4c399d3a18439194
adufou <antoine.du.fou@gmail.com>
2024-06-02 21:39:57 +0200

    chore(vue): add assets

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/minecremental', // from https://stackoverflow.com/questions/74518887/blank-page-when-deploying-a-react-app-to-github-pages-and-vite when doing https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site
     css: {
         postcss: {
             plugins: [tailwind(), autoprefixer()],
