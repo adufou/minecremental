176b39fc965211dd6c83cc8b82a8175bdf8a144d
adufou <antoine.du.fou@gmail.com>
2024-06-03 11:57:14 +0200

    feat(vue): implement Devtools

--- src/modules/Devtools/Devtools.vue
+++ src/modules/Devtools/Devtools.vue
@@ -1,5 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useTickStore } from '@/shared/stores/tick.store';
+import { Button } from '@/shared/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover';
+import { computed, ref } from 'vue';
 
-<template>Devtools</template>
+const tickStore = useTickStore();
+
+const lastTickDuration = ref<number>(0);
+
+const fps = computed(() => {
+    return Math.floor(1000 / lastTickDuration.value);
+});
+
+tickStore.addTickFunction((elapsedTime: number) => {
+    lastTickDuration.value = elapsedTime;
+});
+</script>
+
+<template>
+    <div class="pl-2">
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    class="text-red-500"
+                >
+                    DEV
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent class="w-96">
+                <p>FPS: {{ fps }}</p>
+            </PopoverContent>
+        </Popover>
+    </div>
+</template>
 
 <style scoped></style>
