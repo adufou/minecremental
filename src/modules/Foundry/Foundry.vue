7c720896c5c0788c25b1fd457677c49f0b303094
adufou <antoine.du.fou@gmail.com>
2024-06-09 19:02:08 +0200

    feat(foundry): ui

--- src/modules/Foundry/Foundry.vue
+++ src/modules/Foundry/Foundry.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Furnace from '@/modules/Foundry/components/Furnace.vue';
+import { UiSeparator } from '@/shared/ui/separator';
 </script>
 
 <template>
     <div class="flex flex-col p-2 gap-2 w-full h-full">
         <Furnace />
+        <UiSeparator />
     </div>
 </template>
 
---
+++ src/shared/components/ProgressBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const COLORS = {
+    red: 'bg-red-500',
+    green: 'bg-green-500',
+    blue: 'bg-blue-500',
+    yellow: 'bg-yellow-500',
+    purple: 'bg-purple-500',
+    pink: 'bg-pink-500',
+    indigo: 'bg-indigo-500',
+    teal: 'bg-teal-500',
+    gray: 'bg-gray-500',
+    white: 'bg-white',
+};
+
+const props = withDefaults(
+    defineProps<{
+        value: number;
+        color?: keyof typeof COLORS;
+    }>(),
+    {
+        value: 50,
+        color: 'green',
+    },
+);
+</script>
+
+<template>
+    <div class="w-full h-2">
+        <div
+            :class="`h-full rounded ${COLORS[props.color]}`"
+            :style="{ width: `${props.value}%` }"
+        />
+    </div>
+</template>
+
+<style scoped></style>
